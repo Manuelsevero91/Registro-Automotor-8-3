4a23c3b96fca1e7acac2aa397f24f6dc13b96808
Manuel <manusevero91@gmail.com>
2023-03-08 12:39:52 -0300

    agregue matrix

--- matrix.ts
+++ matrix.ts
@@ -8,12 +8,12 @@ function getConsultarValor(val1,val2){
         const libros = fs.readFileSync('./matrix.json', "utf8");
         const matrix = JSON.parse(libros)
      
-          return matrix[1][1]
+          return matrix[val1][val2]
           
 
     }
     
- console.log(getConsultarValor(1,1));
+ console.log(getConsultarValor(0, 2));
 
    
 
